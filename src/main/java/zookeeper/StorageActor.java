237e4a8762262b04493ca852955bfa447e9a6346
GromkovAlexander <gromkovalex@mail.ru>
2019-12-09 22:53:17 +0300

    Scripted auto-commit on change (2019-12-09 22:53:17) by gitwatch.sh

--- src/main/java/zookeeper/StorageActor.java
+++ src/main/java/zookeeper/StorageActor.java
@@ -1,34 +1,11 @@
 package zookeeper;
 
+import akka.actor.AbstractActor;
 import akka.actor.Actor;
-import akka.actor.ActorContext;
-import akka.actor.ActorRef;
-import scala.PartialFunction;
-import scala.runtime.BoxedUnit;
 
-public class StorageActor extends Actor {
+public class StorageActor extends AbstractActor {
     @Override
-    public void akka$actor$Actor$_setter_$context_$eq(ActorContext x$1) {
-
-    }
-
-    @Override
-    public void akka$actor$Actor$_setter_$self_$eq(ActorRef x$1) {
-
-    }
-
-    @Override
-    public ActorContext context() {
-        return null;
-    }
-
-    @Override
-    public ActorRef self() {
-        return null;
-    }
-
-    @Override
-    public PartialFunction<Object, BoxedUnit> receive() {
+    public Receive createReceive() {
         return null;
     }
 }
