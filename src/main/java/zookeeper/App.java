8e36ead3f8bfd9fc49907b63ec7cf91e77587f79
GromkovAlexander <gromkovalex@mail.ru>
2019-12-09 23:04:57 +0300

    Scripted auto-commit on change (2019-12-09 23:04:57) by gitwatch.sh

--- src/main/java/zookeeper/App.java
+++ src/main/java/zookeeper/App.java
@@ -1,5 +1,26 @@
 package zookeeper;
 
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import akka.http.javadsl.server.AllDirectives;
+import scala.concurrent.Future;
+
+
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+
 public class App {
 
     private final static String ROUTES = "routes";
@@ -11,16 +32,16 @@ public class App {
     private final static String PACKAGE_ID = "packageId";
     private final static int TIME_OUT_MILLS = 10000;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         ActorSystem system = ActorSystem.create(ROUTES);
-        ActorRef mainActor = system.actorOf(Props.create(MainActor.class));
+        ActorRef mainActor = system.actorOf(Props.create(StorageActor.class));
 
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-        TesterJS testerJS = new TesterJS();
+        App testerJS = new App();
 
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = testerJS.testJsRoute(mainActor).flow(system, materializer);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = testerJS.route().flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost(LOCALHOST, LOCALHOST_PORT),
@@ -28,10 +49,23 @@ public class App {
         );
 
         System.out.println(SERVER_ONLINE_MESSAGE);
-        System.in.read();
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         binding
                 .thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound -> system.terminate());
+
+    }
+
+
+    private Route route() {
+        return get(
+                () -> parametr()
+        );
+    }
 
 }
